de40c3f365b7da0776341a2ee27205fc1c67bc35
Daniel Vo <raux221@gmail.com>
2025-03-19 20:40:00 -0700

    lol finished

--- a-frame/clock.js
+++ a-frame/clock.js
@@ -8,7 +8,7 @@ AFRAME.registerComponent("clock", {
     this.titleEl.setAttribute("position", { x: 1, y: 0, z: 0 });
     this.titleEl.setAttribute("color", "#00ff00");
     this.titleEl.setAttribute("font", "sourcecodepro");
-    this.titleEl.setAttribute("value", "Hello, world!");
+    this.titleEl.setAttribute("value", "Check what time it is somewhere else!");
 
     this.el.appendChild(this.titleEl);
 
@@ -16,6 +16,53 @@ AFRAME.registerComponent("clock", {
     //TODO: Use helper functions in timezone.js to display the current time and support converting time zones
     //For implementing interactivity, you may find .addEventListener() useful
     //https://aframe.io/docs/1.2.0/introduction/interactions-and-controllers.html#events
+
+    // Instruction/Time Display
+    this.timeDisplay = document.createElement("a-text");
+    this.timeDisplay.setAttribute("value", "Click a button to see the time!");
+    this.timeDisplay.setAttribute("position", { x: 1, y: -0.4, z: 0 });
+    this.timeDisplay.setAttribute("color", "#00ff00");
+    this.timeDisplay.setAttribute("font", "sourcecodepro");
+    this.el.appendChild(this.timeDisplay);
+
+    // Actually creating the buttons
+    this.createButton("Local Time", { x: 1.25, y: -1, z: 0 }, "local");
+    this.createButton("New York City", { x: 3.5, y: -1, z: 0 }, "est");
+    this.createButton("London", { x: 5.75, y: -1, z: 0 }, "gmt");
+  },
+
+  // Creating buttons and their attributes lol
+  createButton: function (label, position, timezone) {
+    let button = document.createElement("a-plane");
+    button.setAttribute("position", position);
+    button.setAttribute("width", 2);
+    button.setAttribute("height", 0.3);
+    button.setAttribute("color", "#00ff00");
+    button.setAttribute("class", "clickable");
+
+    // Button Text
+    let buttonText = document.createElement("a-text");
+    buttonText.setAttribute("value", label);
+    buttonText.setAttribute("align", "center");
+    buttonText.setAttribute("color", "#ffffff");
+    buttonText.setAttribute("font", "sourcecodepro")
+
+    button.appendChild(buttonText);
+    this.el.appendChild(button);
+
+    // Add Click Event Listener
+    button.addEventListener("click", () => {
+      let now = spacetime.now();
+      let timeString;
+      if (timezone === "local") {
+        timeString = displayTime(now);
+      } else if (timezone === "est") {
+        timeString = displayTime(convertTimeZone(now, "New York"));
+      } else if (timezone === "gmt") {
+        timeString = displayTime(convertTimeZone(now, "GMT"));
+      }
+      this.timeDisplay.setAttribute("value", `Time: ${timeString}`);
+    });
   },
 
   tick: function () {
--- readme.txt
+++ readme.txt
@@ -1,29 +1,32 @@
---Readme document for *author(s)*, *email(s)*, *UCI id(s)*--
+--Readme document for Daniel Vo, danielnv@uci.edu, 82561950--
 
 1. How many assignment points do you believe you completed (replace the *'s with your numbers)?
 
-*/10
-- */1 The ability to display or speak the current local time
-- */3 The ability to convert the current local time to another time zone
-- */1 A demo video demonstrating these features
-- */5 Answers to questions about how the framework supports implementation
-- */0 Any extra credit options implemented
+10/10
+- 1/1 The ability to display or speak the current local time
+- 3/3 The ability to convert the current local time to another time zone
+- 1/1 A demo video demonstrating these features
+- 5/5 Answers to questions about how the framework supports implementation
+- 0/0 Any extra credit options implemented
 
 2. How long, in hours, did it take you to complete this assignment?
-
+    1 hour.
 
 
 3. What online resources did you consult when completing this assignment? (list specific URLs)
-
+    A-frame documentation:
+        https://aframe.io/docs/1.2.0/introduction/interactions-and-controllers.html#events
+        https://aframe.io/docs/1.7.0/introduction/entity-component-system.html
+        
 
 
 4. What classmates or other individuals did you consult as part of this assignment? What did you discuss?
-
+    N/A
 
 
 5. Is there anything special we need to know in order to run your code?
-
+    N/A
 
 
 6. Did you implement any extra credit options? If so, which ones?
-
+    N/A
